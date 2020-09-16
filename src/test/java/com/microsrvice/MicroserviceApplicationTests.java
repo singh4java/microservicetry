6cea8e1ff9eb07b36183d168e7a652d407df7420
Manvendra Singh <Manvendra80386@gmail.com>
2020-09-16 23:05:02 +0530

    first commit

---
+++ src/test/java/com/microsrvice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsrvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
